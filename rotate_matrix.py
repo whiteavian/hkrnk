b52c865b6ab06e5a625588e555b02db2c9fdcb45
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-11-16 16:58:59 -0500

    Fix matrix rotate bugs and add tests.

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -1,8 +1,10 @@
 def rotate_matrix_n_times(mat, m, n, n_times):
     """Rotate each ring of the matrix counterclockwise n_times."""
     while n_times > 0:
-        rotate_matrix(mat, m, n)
+        mat = rotate_matrix(mat, m, n)
         n_times -= 1
+
+    return mat
 
 def rotate_matrix(mat, m, n):
     """Given a matrix and dimensions, rotate counterclockwise each ring."""
@@ -11,16 +13,18 @@ def rotate_matrix(mat, m, n):
         first = mat[indent][indent]
 
         i, j = get_next_indices(indent, indent, m, n, indent)
-        mat[indent][indent] = mat[i][j]
+        mat[indent][indent] = mat[j][i]
 
         # Set each index to the successor value.
-        while (i != indent and j != indent):
+        while not (i == indent and j == indent):
             ip1, jp1 = get_next_indices(i, j, m, n, indent)
-            mat[i][j] = mat[ip1][jp1]
+            mat[j][i] = mat[jp1][ip1]
             i, j = ip1, jp1
 
         # Set the final spot to the first.
         mat[indent + 1][indent] = first
+
+    return mat
 
 def get_next_indices(i, j, m, n, indent):
     """Get the next index pair traversing a matrix clockwise indent from the edge.
@@ -29,13 +33,13 @@ def get_next_indices(i, j, m, n, indent):
     matrix. indent is how far the the edge of the matrix we are traversing."""
 
     # We are on the first row and want the element to the right of the current.
-    if (i + 1 <= n - indent) and j == indent:
+    if (i + 1 < n - indent) and j == indent:
         return i + 1, j
     # We are on the last column and want to get the element beneath the current.
-    elif (j + 1 <= m - indent) and i == n - indent:
+    elif (j + 1 < m - indent) and i == (n - indent - 1):
         return i, j + 1
     # We are on the last row and want to element to the left of the current.
-    elif (i - 1 >= indent) and j == m - indent:
+    elif (i - 1 >= indent) and j == (m - indent - 1):
         return i - 1, j
     # We are on the first column and want the element above the current.
     elif (j - 1 >= indent) and i == indent:
---
+++ test_rotate_matrix.py
@@ -0,0 +1,51 @@
+from rotate_matrix import rotate_matrix, rotate_matrix_n_times
+
+m0 = \
+    [
+        [1, 2],
+        [3, 4],
+    ]
+
+m1 = \
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+    ]
+
+m2 = \
+    [
+        [1, 2],
+        [3, 4],
+        [5, 6],
+    ]
+
+m3 = \
+    [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+    ]
+
+m4 = \
+    [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+        [17, 18, 19, 20],
+    ]
+
+def pprint_matrix(mat):
+    for row in mat:
+        print row
+
+for m in (m0, m1, m2, m3, m4):
+    print "ORIGINAL"
+    pprint_matrix(m)
+    print "NEW MATRIX"
+    pprint_matrix(rotate_matrix(m, len(m), len(m[0])))
+
+pprint_matrix(m0)
+pprint_matrix(rotate_matrix_n_times(m0, 2, 2, 2))
+
