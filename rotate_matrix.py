f234c771933ff8be5475e04aeb6ce7f10fdeb9ef
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-11-17 00:06:05 -0500

    Refactor rotate matrix.

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -1,48 +1,59 @@
-def rotate_matrix_n_times(mat, m, n, n_times):
-    """Rotate each ring of the matrix counterclockwise n_times."""
+def rotate_matrix_n_times(mat, m, n, times):
+    """Rotate each ring of the matrix counterclockwise times."""
     # If we're rotating more than the size of the outer ring, there's no need to go around
     # more than once.
     new_matrix = [[None for i in xrange(n)] for j in xrange(m)]
     for indent in xrange(min(m, n) / 2):
         mod = (2 * (n + m - 4) - (4 * indent))
-        local_n_times = n_times % mod if mod > n_times else n_times
+        local_times = times % mod if mod > times else times
 
         i_fill, j_fill = indent, indent
         i, j = i_fill, j_fill
-        while local_n_times > 0:
-            local_n_times -= 1
-            i, j = get_next_indices(i, j, m, n, indent)
+        i, j = get_next_indices(i, j, m, n, indent, local_times)
 
         new_matrix[j_fill][i_fill] = mat[j][i]
         i_fill, j_fill = get_next_indices(i_fill, j_fill, m, n, indent)
 
         while not (i_fill == indent and j_fill == indent):
-            i, j = get_next_indices(i, j, m, n, indent)
+            i, j = get_next_indices(i, j, m, n, indent, 1)
             new_matrix[j_fill][i_fill] = mat[j][i]
-            i_fill, j_fill = get_next_indices(i_fill, j_fill, m, n, indent)
-
+            i_fill, j_fill = get_next_indices(i_fill, j_fill, m, n, indent, 1)
 
     return new_matrix
 
-def get_next_indices(i, j, m, n, indent):
+def get_next_indices(i, j, m, n, indent, times):
     """Get the next index pair traversing a matrix clockwise indent from the edge.
 
     i, j are the "current" location. m and n are the row and column size of the
     matrix. indent is how far the the edge of the matrix we are traversing."""
 
     # We are on the first row and want the element to the right of the current.
-    if (i + 1 < n - indent) and j == indent:
-        return i + 1, j
-    # We are on the last column and want to get the element beneath the current.
-    elif (j + 1 < m - indent) and i == (n - indent - 1):
-        return i, j + 1
-    # We are on the last row and want to element to the left of the current.
-    elif (i - 1 >= indent) and j == (m - indent - 1):
-        return i - 1, j
-    # We are on the first column and want the element above the current.
-    elif (j - 1 >= indent) and i == indent:
-        return i, j - 1
-    else:
-        print "Don't you wish we were working in a language that had case " \
-              "exhaustivity checking so we could be extra sure we don't need " \
-              "this else?"
+    while times > 0:
+        if (i < n - indent) and j == indent:
+            inc = i + times
+            inc = n - i - 1 if inc > n else inc
+            i, j = i + inc, j
+        # We are on the last column and want to get the element beneath the current.
+        elif (j < m - indent) and i == (n - indent - 1):
+            inc = j + times
+            inc = m - j - 1 if inc > m else inc
+            i, j = i, j + inc
+        # We are on the last row and want to element to the left of the current.
+        elif (i >= indent) and j == (m - indent - 1):
+            inc = i - times
+            inc = i - indent if inc > n else inc
+            i, j = i - inc, j
+        # We are on the first column and want the element above the current.
+        elif (j >= indent) and i == indent:
+            inc = j - times
+            inc = j - indent if inc > m else inc
+            i, j = i, j - inc
+        else:
+            print "Don't you wish we were working in a language that had case " \
+                  "exhaustivity checking so we could be extra sure we don't need " \
+                  "this else?"
+
+        times -= inc
+        assert times >= 0
+
+    return i, j
