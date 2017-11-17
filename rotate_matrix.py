53d5b3ddfdd76df61aa6239c2b1a7affb937d779
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-11-17 00:38:24 -0500

    Fix rotate matrix bugs checkpoint.

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -8,11 +8,10 @@ def rotate_matrix_n_times(mat, m, n, times):
         local_times = times % mod if mod > times else times
 
         i_fill, j_fill = indent, indent
-        i, j = i_fill, j_fill
-        i, j = get_next_indices(i, j, m, n, indent, local_times)
+        i, j = get_next_indices(i_fill, j_fill, m, n, indent, local_times)
 
         new_matrix[j_fill][i_fill] = mat[j][i]
-        i_fill, j_fill = get_next_indices(i_fill, j_fill, m, n, indent)
+        i_fill, j_fill = get_next_indices(i_fill, j_fill, m, n, indent, 1)
 
         while not (i_fill == indent and j_fill == indent):
             i, j = get_next_indices(i, j, m, n, indent, 1)
@@ -29,24 +28,20 @@ def get_next_indices(i, j, m, n, indent, times):
 
     # We are on the first row and want the element to the right of the current.
     while times > 0:
-        if (i < n - indent) and j == indent:
-            inc = i + times
-            inc = n - i - 1 if inc > n else inc
+        if (i < n - indent - 1) and j == indent:
+            inc = n - i if i + times >= n else times
             i, j = i + inc, j
         # We are on the last column and want to get the element beneath the current.
-        elif (j < m - indent) and i == (n - indent - 1):
-            inc = j + times
-            inc = m - j - 1 if inc > m else inc
+        elif (j < m - indent - 1) and i == (n - indent - 1):
+            inc = m - j - 1 if j + times >= m else times
             i, j = i, j + inc
         # We are on the last row and want to element to the left of the current.
-        elif (i >= indent) and j == (m - indent - 1):
-            inc = i - times
-            inc = i - indent if inc > n else inc
+        elif (i > indent) and j == (m - indent - 1):
+            inc = i - indent if i - times < indent else times
             i, j = i - inc, j
         # We are on the first column and want the element above the current.
-        elif (j >= indent) and i == indent:
-            inc = j - times
-            inc = j - indent if inc > m else inc
+        elif (j > indent) and i == indent:
+            inc = j - indent if j - times < indent else times
             i, j = i, j - inc
         else:
             print "Don't you wish we were working in a language that had case " \
