1c5eec36e7cdec28e0e2a7f570790fb3fc3fc7dc
Laura White-Avian <whiteavian@users.noreply.github.com>
2017-11-17 18:29:15 -0500

    Add general insert sort.

--- insert_sort.py
+++ insert_sort.py
@@ -1,19 +1,22 @@
-def insert_sort(ar, n):
+def insert_sort_single(ar, n):
     i = n - 1
     e = ar[i]
 
     while i > 0:
         if ar[i - 1] > e:
             ar[i] = ar[i - 1]
-            print(" ".join(map(str, ar)))
             i -= 1
         else:
             ar[i] = e
-            print(" ".join(map(str, ar)))
-            return None
+            return ar
 
     ar[0] = e
-    print(" ".join(map(str, ar)))
+    return ar
+
+def insert_sort(ar, n):
+    for i in range(1, n):
+        ar = insert_sort_single(ar[:i + 1], i + 1) + ar[i + 1:]
+        print(" ".join(map(str, ar)))
 
 print("NEW")
 insert_sort([1, 2, 3, 4, 5, 3], 6)
@@ -29,3 +32,5 @@ print("NEW")
 insert_sort([1, 1], 2)
 print("NEW")
 insert_sort([3], 1)
+print("NEW")
+insert_sort([4, 3, 2, 1, 8], 5)
